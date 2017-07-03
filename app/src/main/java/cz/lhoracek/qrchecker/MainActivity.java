0117e7a5c8d80297a1bb003d02bfead42bf8bf4f
Lubos Horacek <lubos@iflix.com>
2017-07-03 18:51:06 +0200

    Checking orientation

--- app/src/main/java/cz/lhoracek/qrchecker/MainActivity.java
+++ app/src/main/java/cz/lhoracek/qrchecker/MainActivity.java
@@ -2,15 +2,23 @@ package cz.lhoracek.qrchecker;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.databinding.DataBindingUtil;
 import android.graphics.PointF;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -35,10 +43,37 @@ public class MainActivity extends AppCompatActivity implements QRCodeReaderView.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         checkPermission();
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mBinding.setQrListener(this);
+
+
+        SensorManager sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(new SensorEventListener() {
+            int orientation=-1;
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (event.values[1]<6.5 && event.values[1]>-6.5) {
+                    if (orientation!=1) {
+                       Timber.d("Sensor Landscape");
+                    }
+                    orientation=1;
+                } else {
+                    if (orientation!=0) {
+                        Timber.d("Sensor Portrait");
+                    }
+                    orientation=0;
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                // TODO Auto-generated method stub
+
+            }
+        }, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
     }
 
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 234567;
@@ -74,5 +109,11 @@ public class MainActivity extends AppCompatActivity implements QRCodeReaderView.
     @Override
     public void onQRCodeRead(String text, PointF[] points) {
         Timber.d("QrCode read %s", text);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Timber.d("Configuration changed :  %s", newConfig.orientation == Configuration.ORIENTATION_PORTRAIT ? "Portrait" : "Landscape");
     }
 }
